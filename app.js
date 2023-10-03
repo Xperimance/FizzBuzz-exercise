6af48bf05cb924f7c19c916f7205b7d24bd3a60f
Dale Hendricks <137794719+Xperimance@users.noreply.github.com>
2023-10-03 15:04:39 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+// When a user inputs a Number
+// Loop from 1 to the entered Number
+// If the current number is divisible by 3 then print "Fizz"
+// If the current number is divisible by 5 then print "Buzz"
+// If the current number is divisible by both 3 and 5 then print "FizzBuzz"
+// Otherwise print the current number
+
+let answer =parseInt(prompt("Please enter the number you would like to FizzBuzz up to:  ")); //parseInt returns a number from the user input
+
+for (let i = 1; i <= answer; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+}
